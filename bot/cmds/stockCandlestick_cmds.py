967c8f0919355e5c354cfd0b5a53200025abc0b8
DidierRLopes <dro.lopes@campus.fct.unl.pt>
2023-09-09 18:19:14 -0700

    make candle command relying on itself

--- bot/cmds/stockCandlestick_cmds.py
+++ bot/cmds/stockCandlestick_cmds.py
@@ -1,10 +1,15 @@
 import traceback
 
+from openbb import obb
+
 import disnake
 from disnake.ext import commands
+from datetime import datetime, timedelta
 
 from bot.helpers import chart_response
 from bot.showview import ShowView
+from models.api_models import MainModel
+from utils.pywry_figure import PyWryFigure
 
 
 class CandlestickChartsCommands(commands.Cog):
@@ -13,11 +18,28 @@ class CandlestickChartsCommands(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.slash_command(name="cd")
-    async def cd(
+    @commands.slash_command(name="candle")
+    async def candle(
         self,
         inter: disnake.AppCmdInter,
         ticker: str,
+        interval: str = commands.Param(
+            choices=[
+                "1day",
+                "15min",
+                "5min",
+            ],
+            default="1day",
+        ),
+        days: int = 200,
+        provider: str = commands.Param(
+            choices=[
+                "fmp",
+                "polygon",
+                "intrinio"
+            ],
+            default="fmp",
+        ),
     ):
         """Shows a daily candlestick chart for the ticker provided.
 
@@ -29,60 +51,51 @@ class CandlestickChartsCommands(commands.Cog):
         try:
             await inter.response.defer()
 
-            response = chart_response(ticker, "1day", 200)
+            # Pre-processing of parameters
+            ticker = ticker.upper()            
+            params = {
+                "symbol": ticker,
+                "provider": provider,
+                "start_date":  (datetime.now() - timedelta(days=days)).strftime("%Y-%m-%d"),
+                "end_date": datetime.now().strftime("%Y-%m-%d"),
+                "interval": interval,
+                "chart": True,
+            }
+
+            # Get the data
+            data = obb.stocks.load(**params).chart.content
+
+            # Format for display
+            title = f"{ticker} {interval.replace('1day', 'Daily')}"
+
+            fig = (
+                PyWryFigure()
+                .update(data)
+                .update_layout(
+                    margin=dict(l=80, r=10, t=40, b=20),
+                    paper_bgcolor="#111111",
+                    plot_bgcolor="rgba(0,0,0,0)",
+                    height=762,
+                    width=1430,
+                    title=dict(text=title, x=0.5),
+                    xaxis=dict(tick0=0.5, tickangle=0),
+                )
+            )
+
+            y_min, y_max = min(fig.data[0].low), max(fig.data[0].high)
+            y_range = y_max - y_min
+            y_min -= y_range * 0.2
+            y_max += y_range * 0.08
+
+            fig.update_layout(yaxis=dict(range=[y_min, y_max], autorange=False))
+
+            response: dict = {"plots": fig.prepare_image()}
 
         except Exception as e:
             traceback.print_exc()
-            return await ShowView().discord(inter, "cd", str(e), error=True)
+            return await ShowView().discord(inter, "candle", str(e), error=True)
 
-        await ShowView().discord(inter, "cd", response, no_embed=True)
-
-    @commands.slash_command(name="cc")
-    async def cc(
-        self,
-        inter: disnake.AppCmdInter,
-        ticker: str,
-    ):
-        """Shows a 5 minute candlestick chart for the ticker provided.
-
-        Parameters
-        -----------
-        ticker: Stock Ticker
-        """
-
-        try:
-            await inter.response.defer()
-
-            response = chart_response(ticker, "5min", 4)
-
-        except Exception as e:
-            traceback.print_exc()
-            return await ShowView().discord(inter, "cc", str(e), error=True)
-
-        await ShowView().discord(inter, "cc", response, no_embed=True)
-
-    @commands.slash_command(name="c15m")
-    async def c15m(
-        self,
-        inter: disnake.AppCmdInter,
-        ticker: str,
-    ):
-        """Shows a 15 minute candlestick chart for the ticker provided.
-
-        Parameters
-        -----------
-        ticker: Stock Ticker
-        """
-        try:
-            await inter.response.defer()
-
-            response = chart_response(ticker, "15min", 4)
-
-        except Exception as e:
-            traceback.print_exc()
-            return await ShowView().discord(inter, "c15m", str(e), error=True)
-
-        await ShowView().discord(inter, "c15m", response, no_embed=True)
+        await ShowView().discord(inter, "candle", response, no_embed=True)
 
 
 def setup(bot: commands.Bot):
